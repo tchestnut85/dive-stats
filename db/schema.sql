9cb4ec65db4197302bfd27cd84ab34c2f07b8ba8
Tom Chestnut <tchestnut85@gmail.com>
2022-06-20 11:44:36 -0400

    added additional tables and custom postgres function

--- db/schema.sql
+++ db/schema.sql
@@ -20,3 +20,36 @@ CREATE TABLE divers (
   is_instructor BOOLEAN DEFAULT FALSE NOT NULL,
   certification_id INTEGER REFERENCES certifications(id) ON DELETE SET NULL
 );
+
+CREATE DOMAIN LATLONG AS POINT
+  CHECK (VALUE[0] BETWEEN -90 AND 90)
+  CHECK (VALUE[1] BETWEEN -180 AND 180);
+
+CREATE TABLE locations (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+  coordinates LATLONG NOT NULL
+);
+
+CREATE TABLE tags (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+  location_id INTEGER NOT NULL REFERENCES locations(id) ON DELETE CASCADE,
+  UNIQUE(name, location_id)
+);
+
+CREATE TABLE dives (
+  id SERIAL PRIMARY KEY,
+  depth NUMERIC(5, 2) NOT NULL,
+  dive_date TIMESTAMP NOT NULL DEFAULT NOW(),
+  duration UNSIGNED NOT NULL,
+  diver_id INTEGER NOT NULL REFERENCES divers(id) ON DELETE CASCADE,
+  location_id INTEGER NOT NULL REFERENCES locations(id) ON DELETE CASCADE
+);
+
+CREATE FUNCTION random_between(low INT, high INT)
+RETURNS INT AS $$
+BEGIN
+  RETURN FLOOR(RANDOM() * (high - low + 1) + low);
+END;
+$$ LANGUAGE plpgsql;
--- db/seeds.sql
+++ db/seeds.sql
@@ -18,4 +18,38 @@ VALUES
   ('Kendall', 'Rivers', TRUE, 4),
   ('Mira', 'Waters', FALSE, 5);
 
+INSERT INTO locations (name, coordinates)
+VALUES
+  ('Barracude Point', POINT(4.112742, 118.631106)),
+  ('Cape Kri', POINT(-0.556324, 130.690725)),
+  ('SS Yongala', POINT(-19.304703, 147.623977)),
+  ('Great Blue Hole', POINT(17.316007, -87.535225)),
+  ('Richelieu Rock', POINT(9.362752, 98.021727));
+
+INSERT INTO tags (location_id, name)
+VALUES
+  (1, 'wall'),
+  (2, 'sharks'),
+  (2, 'current'),
+  (3, 'wreck'),
+  (3, 'sharks'),
+  (5, 'sharks'),
+  (5, 'reef'),
+  (5, 'current');
+
+INSERT INTO locations (name, coordinates)
+SELECT
+  CONCAT('Unknown locations #', loc_id),
+  POINT(random_between(-90, 90), random_between(-180, 180))
+FROM generate_series(1, 50) AS loc_id;
+
+INSERT INTO dives (depth, dive_date, duration, diver_id, location_id)
+SELECT
+  random_between(1, 300) * RANDOM(),
+  NOW() - ('1 day'::INTERVAL*random_between(1, 1000)),
+  random_between(1, 720),
+  random_between(1, 5),
+  random_between(1, 5)
+FROM generate_series(1, 1000);
+
 COMMIT;
